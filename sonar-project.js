5afdef6b13b4ecaa1d859c00376ae0172c679cd6
Surya Kiran Ch <suryakiran1648@gmail.com>
2023-12-23 14:15:51 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://13.235.23.48:9000/',
+  serverUrl: 'http://13.201.136.137:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
